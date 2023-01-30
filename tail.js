9bacbaf25e1bfa5e981790fe679296d91101683b
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-01-30 00:19:26 -0500

    Tail Function

---
+++ tail.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  let arrayTail = [];
+  for (let i = 1; i < array.length; i++) {
+    arrayTail.push(array[i]);
+  }
+  return arrayTail;
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+console.log(tail(words)); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+const words2 = ["Yo Yo"];
+console.log(tail(words2)); // no need to capture the return value since we are not checking it
+assertEqual(words2.length, 1); // original array should still have 1 elements!
+
+const words3 = [];
+console.log(tail(words3)); // no need to capture the return value since we are not checking it
+assertEqual(words3.length, 0); // original array should still have 0 elements!
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
