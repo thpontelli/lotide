596cd15f6cb3118cb9f9d5471409a3620243f2cf
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-02-02 19:13:09 -0500

    middle function

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const middle = function(array) {
+  let middleArray = []
+  if (array.length <= 2) {
+  } else if (array.length % 2 !== 0) {
+    middleArray.push(array[Math.floor(array.length / 2)]);  
+  } else {
+    middleArray.push(array[array.length / 2 - 1]);
+    middleArray.push(array[array.length / 2]);
+  }
+  return middleArray
+};
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
