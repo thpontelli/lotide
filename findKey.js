1138bc16d13e242d48039b1c12df4830d92a558e
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-02-06 20:16:56 -0500

    Implementing findKey function

---
+++ findKey.js
@@ -0,0 +1,45 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  let objKeys = Object.keys(obj);         // Array of the Keys of the Object
+  for (const objKey of objKeys) {         // iterating through the keys of the object
+    //console.log(objKey);
+    //console.log(obj[objKey].stars);
+    if (callback(obj[objKey])) {          // calling callback fn using as argument the object key
+      return objKey;
+    }
+  }
+};
+
+const newObj = {
+  HarryPotter: "Daniel Radcliffe",
+  HermioneGranger: "Emma Watson",
+  RonyWeasley: "Rupert Grint",
+  Dumbledore: "Michael Gambon"
+};
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)); // => "noma"
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey(newObj, x => x === "Emma Watson"), "HermioneGranger");
