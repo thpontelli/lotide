b9864f829e999be9c7f374a8a4799d1c5f2e5a06
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-02-17 21:22:33 -0500

    refactoring assertEqual and head

--- head.js
+++ head.js
@@ -1,18 +1,10 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 
 const head = function(array) {
   return array[0]
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
-assertEqual(head([2]), 2);
+module.exports = head;
 
 
