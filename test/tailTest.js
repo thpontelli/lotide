2074f6417afc67221d912ef446a94e09bd5e98e8
Thiago Pontelli <thiago.pontelli@gmail.com>
2023-02-18 10:46:20 -0500

    middleTest.js and tailTest.js with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,40 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// console.log(tail(words)); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const words2 = ["Yo Yo"];
+// console.log(tail(words2)); // no need to capture the return value since we are not checking it
+// assertEqual(words2.length, 1); // original array should still have 1 elements!
+
+// const words3 = [];
+// console.log(tail(words3)); // no need to capture the return value since we are not checking it
+// assertEqual(words3.length, 0); // original array should still have 0 elements!
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-console.log(tail(words)); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it('returns ["Lighthouse", "Labs"] for ["Yo Yo", "Lighthouse", "Labs"];', () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
 
-const words2 = ["Yo Yo"];
-console.log(tail(words2)); // no need to capture the return value since we are not checking it
-assertEqual(words2.length, 1); // original array should still have 1 elements!
+  it('returns [] for ["Yo Yo"];', () => {
+    assert.deepEqual(tail(["Yo Yo"]), []);
+  });
 
-const words3 = [];
-console.log(tail(words3)); // no need to capture the return value since we are not checking it
-assertEqual(words3.length, 0); // original array should still have 0 elements!
+  it('returns [] for [];', () => {
+    assert.deepEqual(tail([]), []);
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+});
+
+
+
